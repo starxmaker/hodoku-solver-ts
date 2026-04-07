3714d9b3c339f6f42544050c17f6db4904da0cf7
starxmaker <beneath.black.skies@gmail.com>
2026-04-06 23:29:30 -0400

    fix: more parity

--- src/solver/UniquenessSolver.ts
+++ src/solver/UniquenessSolver.ts
@@ -52,10 +52,8 @@ export class UniquenessSolver extends AbstractSolver {
   }
 
   private _findUR1(): SolutionStep | null {
-    // Iterate over every pair of rows
     for (let r1 = 0; r1 < 8; r1++) {
       for (let r2 = r1 + 1; r2 < 9; r2++) {
-        // Iterate over every pair of columns
         for (let c1 = 0; c1 < 8; c1++) {
           for (let c2 = c1 + 1; c2 < 9; c2++) {
             const i11 = r1 * 9 + c1;
@@ -88,7 +86,7 @@ export class UniquenessSolver extends AbstractSolver {
                   }
                 }
                 if (twoOnly === 3 && extraIndex !== -1) {
-                  const toDelete = [];
+                  const toDelete: { index: number; value: Digit }[] = [];
                   if (this.sudoku.isCandidate(extraIndex, a))
                     toDelete.push({ index: extraIndex, value: a as Digit });
                   if (this.sudoku.isCandidate(extraIndex, b))
